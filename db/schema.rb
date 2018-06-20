6aa72ef5144760a4590a2065660e71513a427342
Philip Hearn <zero.zain@gmail.com>
2018-06-20 15:20:19 -0400

    Upgraded deprecated gems. Set heroku to v16

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027032350) do
+ActiveRecord::Schema.define(version: 2016_10_27_032350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "quotes", force: true do |t|
-    t.string   "saying"
-    t.string   "author"
+  create_table "quotes", id: :serial, force: :cascade do |t|
+    t.string "saying"
+    t.string "author"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
